759621f374fa45a5f7fe52a1cc04503e1fdf1191
dalveernayak <dnayak009@gmail.com>
2024-03-17 14:48:40 +0530

    hello this is first commit2

---
+++ README.md
@@ -0,0 +1,14 @@
+# Post Page Notification Plugin
+
+This WordPress plugin sends an email notification to the site admin whenever a new post or page is published.
+
+## Installation
+
+1. Upload the `post-page-notification` directory to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+## Usage
+
+Once activated, the plugin will automatically send an email notification to the site admin whenever a new post or page is published.
+
+## Credits
---
+++ post-page-notification.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name: Post Page Notification
+ * Plugin URI: https://shiksha-kendra.in/
+ * Description: Sends notification to admin when a new post or page is published.
+ * Version: 6.0
+ * Author: Nayak
+ * Author URI: https://shiksha-kendra.in/
+ * License: GPL2
+ */
+
+// Plugin code will go here
+
+// Send notification to admin when new post or page is published
+function send_notification_on_publish($ID, $post) {
+    $admin_email = get_option('admin_email');
+    $subject = 'New Post/Page Published';
+    $message = 'A new post or page has been published: ' . $post->post_title;
+    wp_mail($admin_email, $subject, $message);
+}
+add_action('publish_post', 'send_notification_on_publish', 10, 2);
+add_action('publish_page', 'send_notification_on_publish', 10, 2);
+//////////////////////////////////////////
+//i am adding the code for testing hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh hhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+
+
+?>
